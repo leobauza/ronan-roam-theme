e9101a763b0c0f16b629f53cb6efec05fc1b7e86
Leo Bauza <leonardobauza@gmail.com>
2021-02-27 15:19:40 -0500

    [#5] first draft of sidebar and add tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,19 @@
 const postcssPresetEnv = require('postcss-preset-env')
+const cssnano = require('cssnano')
 
 module.exports = ({ options }) => {
   let plugins = [
     require('postcss-easy-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
     postcssPresetEnv({
       features: {
         'custom-media-queries': true,
         'nesting-rules': true,
       },
+    }),
+    cssnano({
+      preset: 'default',
     }),
   ]
 
