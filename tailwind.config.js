86f14bc1f16691d4385df371b5df5a7aee697e01
Leo Bauza <leonardobauza@gmail.com>
2021-03-01 17:50:53 -0500

    [#14] style blocks and start styling references (#15)

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         20: '20deg',
         '-20': '-20deg',
       },
+      boxShadow: {
+        outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
+      },
     },
   },
   variants: {
